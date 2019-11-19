6c6db9168610ca4b4b702b1250da17cc1474df4a
Michael Hartnett <michael.hartnett@swri.org>
2019-11-19 14:53:51 -0600

    new header

--- src/NATS/scriptsSwRI/NATS_Python_Header.py
+++ src/NATS/scriptsSwRI/NATS_Python_Header.py
@@ -32,6 +32,48 @@ classpath = classpath + ":" + str_NATS_HOME + "dist/nats-shared.jar"
 classpath = classpath + ":" + str_NATS_HOME + "dist/json.jar"
 classpath = classpath + ":" + str_NATS_HOME + "dist/commons-logging-1.2.jar"
 
+class NATS_Config:
+    def __init__(self, duration = 86400,
+                 interval = 1,
+                 client_dir = client,
+                 wind_dir = 'share/tg/rap',
+                 track_file = "share/tg/trx/swim_example_aug.trx",
+                 max_flt_lev_file = "share/tg/trx/swim_example_mfl.trx"):
+        self.endTime = duration
+        self.interval = interval
+        self.initializeJVM()
+        self.wind_dir = wind_dir
+        self.trx_file = track_file
+        self.mfl_file = max_flt_lev_file
+
+    '''Initialize the NATS client'''
+    def initializeJVM(self):
+
+        self.NATS_SIMULATION_STATUS_ENDED = NATS_SIMULATION_STATUS_ENDED;
+        clsNATSStandalone = JClass('NATSStandalone')
+
+        # Start NATS Standalone environment
+        self.natsClient = clsNATSStandalone.start()
+
+        self.equipmentInterface = self.natsClient.getEquipmentInterface()
+
+        # Get EnvironmentInterface
+        self.environmentInterface = self.natsClient.getEnvironmentInterface()
+        # Get AirportInterface
+        self.airportInterface = self.environmentInterface.getAirportInterface()
+        # Get TerminalAreaInterface
+        self.terminalAreaInterface = self.environmentInterface.getTerminalAreaInterface()
+
+        self.sim = self.natsClient.getSimulationInterface()
+
+        self.aircraftInterface = self.equipmentInterface.getAircraftInterface()
+
+        self.entityInterface = self.natsClient.getEntityInterface()
+
+        self.controllerInterface = self.entityInterface.getControllerInterface()
+
+        self.pilotInterface = self.entityInterface.getPilotInterface()
+
 startJVM(getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % classpath)
 
 # Flight phase value definition
